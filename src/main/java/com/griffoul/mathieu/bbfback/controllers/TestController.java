636b167346d10dc6fc2d50e7c65b1cbb3e37a042
mathieu_griffoul <mgriffoul@oui.sncf>
2019-10-18 18:13:01 +0200

    Add https and CORS authorization for bbf front

--- src/main/java/com/griffoul/mathieu/bbfback/controllers/TestController.java
+++ src/main/java/com/griffoul/mathieu/bbfback/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.griffoul.mathieu.bbfback.controllers;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +15,10 @@ import java.util.Optional;
 @RequestMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
 
+    @CrossOrigin(origins = "https://localhost:3000")
     @GetMapping(value = "/authent")
     public HttpEntity<String> testController(@RequestParam(value = "login") final String login) {
+        System.out.println("+1");
         return ResponseEntity.of(Optional.of("Bonjour "+login));
     }
 
